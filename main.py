8a7a76ab8da5ee14ad41d24f74984c7cd306343f
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-16 17:59:42 +0900

    Tensorflow and ReinforcementLearning imports

--- main.py
+++ main.py
@@ -2,6 +2,14 @@
 import gym
 import random
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.optimizers import Adam
+
+from rl.agents import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
 environment = gym.make("CartPole-v1", render_mode="human")
 
 episodes = 10

d382774ec75d3d06af3eb34ac23db83328d49fb2
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-16 16:58:38 +0900

    Basic random agent for Cartpole-v1 environment in Gym

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # Deep-Reinforcement-Learning using OpenAI Gym & Tensorflow
+import gym
+import random
+
+environment = gym.make("Cartpole-v1", render_mode="human")
+
+episodes = 10
+for episode in range(1, episodes+1):
+    state = environment.reset()
+    done = False
+    score = 0
+
+    while not done:
+        action = random.choice([0, 1])
+        _, = environment.step(action)

d7808b9dcc91fb2f3a4fe5f4b8132b63388b4163
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-16 19:01:59 +0900

    Brand new TF-Agent imports

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 import numpy as np
 import tensorflow as tf
 
-
-
-
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.networks import q_network
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.utils import common
+from tf_agents.drivers import dynamic_step_driver
+from tf_agents.policies import policy_saver
+from tf_agents.trajectories import trajectory
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.metrics import tf_metrics
